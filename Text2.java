38fa0970e5020476cc40525e6fe172bbb2bd4067
Rossosaurs <Ross.Rawlings01@gmail.com>
2016-09-29 10:19:46 -0700

    Add files via upload

---
+++ Text2.java
@@ -0,0 +1,26 @@
+class Text2 {
+
+	public static void main (String a[]){
+			int i,ci;   // integer i
+			char c;   // charater c
+	
+	i= 7;
+	c = 'A';
+	String s = "RIP harambe";
+	System.out.println("-------------------------");
+	System.out.println(i = +i);
+	System.out.println(c = +c);
+	System.out.println(s = +s);
+		System.out.println("-------------------------");
+	//do some math on the variables
+	i = i % 7;
+	ci =(int)c %7;
+	
+	System.out.println("-------------------------");
+	System.out.println(i);
+	System.out.println(ci);
+	
+	}
+	
+}
+	
