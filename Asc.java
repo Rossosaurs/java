2553abaf6893616a6394ece69acc8d2554b47db6
Rossosaurs <Ross.Rawlings01@gmail.com>
2016-09-29 10:16:13 -0700

    Add files via upload

---
+++ Asc.java
@@ -0,0 +1,34 @@
+// file: Asc.java
+// javac Asc.java
+// java Asc
+class Asc {
+	public static void main(String a[]){
+		//varibles
+		int i;// interger i
+		char c;// character c
+		//for lopp
+		for (i = 0; i < 256; i++){
+		c =(char)i;
+		System.out.println(i);
+		System.out.println(" ");
+		System.out.println(c); 
+			System.out.println(" ");	
+	
+	
+	
+	
+	}
+	
+	
+	
+	
+	
+	}
+
+
+
+
+}
+
+
+
